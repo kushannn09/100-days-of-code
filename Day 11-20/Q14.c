225a002243df817aa6879aa7ac9c2d94e4338e97
kushannn09 <piyush1220066@gmail.com>
2025-10-08 17:15:02 +0530

    Create Q14.c

---
+++ Day 11-20/Q14.c
@@ -0,0 +1,26 @@
+/*Q14: Write a program to input a character and check whether it is a vowel or consonant using if–else.
+*/
+
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = ch + ('a' - 'A');
+        }
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            printf("Vowel\n");
+        } else {
+            printf("Consonant\n");
+        }
+    } else {
+        printf("Invalid input. Please enter an alphabet character.\n");
+    }
+
+    return 0;
+}
+
