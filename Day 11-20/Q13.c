5eef8bdafa9651a23d918949bdb9fda50eeaa1e5
kushannn09 <piyush1220066@gmail.com>
2025-10-08 17:16:13 +0530

    Update Q13.c

--- Day 11-20/Q13.c
+++ Day 11-20/Q13.c
@@ -6,6 +6,11 @@ int main() {
     int year;
     printf("Enter a year: ");
     scanf("%d", &year);
-    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
         printf("Leap year\n");
     } else {
+        printf("Not a leap year\n");
+    }
+    return 0;
+}
+
