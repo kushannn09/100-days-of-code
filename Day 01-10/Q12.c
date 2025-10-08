773b0efb3750ed62d24d5682bc1f7edfea2f1ed9
kushannn09 <piyush1220066@gmail.com>
2025-10-08 18:38:18 +0530

    Create Q12.c

---
+++ Day 01-10/Q12.c
@@ -0,0 +1,18 @@
+/*Q12: Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+*/
+
+#include <stdio.h>
+int main() {
+    int number;
+    scanf("%d", &number);
+    if (number >= 0) {
+        if (number == 0) {
+            printf("Zero\n");
+        } else {
+            printf("Positive\n");
+        }
+    } else {
+        printf("Negative\n");
+    }
+    return 0;
+}
