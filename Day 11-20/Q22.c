e2a5d66b827269f3fe04fd73100f0586bffcd314
kushannn09 <piyush1220066@gmail.com>
2025-10-19 11:46:12 +0530

    Update Q22.c

--- Day 11-20/Q22.c
+++ Day 11-20/Q22.c
@@ -1,19 +1,24 @@
-/*Q12: Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+/*Q22: Write a program to find profit or loss percentage given cost price and selling price.
 */
-
 #include <stdio.h>
 int main() {
-    int num;
-    scanf("%d", &num);
-    if (num >= 0) {
-        if (num == 0) {
-            printf("Zero\n");
-        } else {
-            printf("Positive\n");
-        }
+    float costPrice, sellingPrice, amount, percentage;
+    printf("Enter Cost Price: ");
+    scanf("%f", &costPrice);
+    printf("Enter Selling Price: ");
+    scanf("%f", &sellingPrice);
+    if (sellingPrice > costPrice) {
+        amount = sellingPrice - costPrice;
+        percentage = (amount / costPrice) * 100;
+        printf("Profit: %.2f%%\n", percentage);
+    } else if (costPrice > sellingPrice) {
+        amount = costPrice - sellingPrice;
+        percentage = (amount / costPrice) * 100;
+        printf("Loss: %.2f%%\n", percentage);
     } else {
-        printf("Negative\n");
+        printf("No Profit, No Loss\n");
     }
-
     return 0;
 }
+
+
