e15b306fa4545b2f04d89ff7fde19bd965b33ddc
kushannn09 <piyush1220066@gmail.com>
2025-10-26 10:35:03 +0530

    Create Q40.c

---
+++ Day 11-20/Q40.c
@@ -0,0 +1,15 @@
+/*Q40: Write a program to find the 1’s complement of a binary number and print it.
+*/
+#include <stdio.h>
+int main() {
+    char binary[100];
+    scanf("%s", binary);
+    for(int i = 0; binary[i] != '\0'; i++) {
+        if(binary[i] == '0')
+            printf("1");
+        else if(binary[i] == '1')
+            printf("0");
+    }
+    printf("\n");
+    return 0;
+}
