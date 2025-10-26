594bad3d61980411ba49670ecd6e898ada7bf371
kushannn09 <piyush1220066@gmail.com>
2025-10-26 09:48:08 +0530

    Update Q26.c

--- Day 11-20/Q26.c
+++ Day 11-20/Q26.c
@@ -1,17 +1,14 @@
-/*Q16: Write a program to input three numbers and find the largest among them using if–else.
+/*Q26: Write a program to print numbers from 1 to n.
 */
-
 #include <stdio.h>
 int main() {
-    int a, b, c;
-    printf("Enter three numbers: ");
-    scanf("%d %d %d", &a, &b, &c);
-    if (a >= b && a >= c) {
-        printf("Largest is %d\n", a);
-    } else if (b >= a && b >= c) {
-        printf("Largest is %d\n", b);
-    } else {
-        printf("Largest is %d\n", 
+    int n;
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++) {
+        printf("%d", i);
+        if(i < n)
+            printf(" ");
     }
+    printf("\n");
     return 0;
 }
