59033d067faf93d49e0541702077bc50a59c9da0
kushannn09 <piyush1220066@gmail.com>
2025-10-27 18:25:37 +0530

    Create Q42.c

---
+++ Day 21-30/Q42.c
@@ -0,0 +1,16 @@
+/*Q42: Write a program to check if a number is a perfect number.
+*/
+#include <stdio.h>
+int main() {
+    int num, sum = 0;
+    scanf("%d", &num);
+    for (int i = 1; i <= num / 2; i++) {
+        if (num % i == 0)
+            sum += i;
+    }
+    if (sum == num)
+        printf("Perfect number");
+    else
+        printf("Not perfect number");
+    return 0;
+}
