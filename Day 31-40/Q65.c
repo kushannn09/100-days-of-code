276cea03c3d274a8210339fda9467df2505a2629
kushannn09 <piyush1220066@gmail.com>
2025-10-28 18:14:38 +0530

    Create Q65.c

---
+++ Day 31-40/Q65.c
@@ -0,0 +1,25 @@
+/*Q65: Search in a sorted array using binary search.
+*/
+#include <stdio.h>
+int main() {
+    int n, key;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    scanf("%d", &key);
+    int left = 0, right = n - 1, found = -1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == key) {
+            found = mid;
+            break;
+        } else if (arr[mid] < key) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    if (found != -1) printf("Found at index %d\n", found);
+    else printf("-1\n");
+    return 0;
+}
